d05eec4d382ce3a2844d939edcac0a33890510bf
Abdul Wassay Ansari <abdulwassayansari786@gmail.com>
2021-07-09 18:10:02 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    posts:[ ]
+  }
+  componentDidMount(){
+    axios.get('https://api.covidtracking.com/v1/states/current.json')
+    .then(res => {
+      console.log(res)
+      this.setState({
+        posts: res.data.slice(0,10)
+      })
+    })
+
+  }
+  render(){
+    const { posts} = this.state;
+    const postList = posts.length ? (
+      posts.map(post => {
+        return(
+          <div className='post card' key={post.id}>
+            <div className='card-content'>
+              <br></br>
+              <p> State: {post.state}</p>
+              <p> Date: {post.date}</p>
+              <p> Positive: {post.positive}</p>
+              <p> Deaths: {post.death}</p>
+
+            </div>
+
+          </div>
+        )
+      })
+    ) : (
+      <div className="center"></div>
+    ) 
+    return(
+      <div className='container'>
+        <h4 className='center' ></h4>
+       {postList}
+
+      </div>
+    )
+  }
 }
 
 export default App;
+
